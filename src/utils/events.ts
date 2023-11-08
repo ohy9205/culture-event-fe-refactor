db79585c3065a550cfb05afcfc4fc1c1f9084158
minki <93minki@minkiui-MacBookAir.local>
2023-11-08 17:06:09 +0900

    feat: 테스트용 API URL 추가

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,5 +1,10 @@
-import { SimpleEventList } from "./../types/events";
 import { Event, EventThumbnail } from "../types/events";
+import { SimpleEventList } from "./../types/events";
+
+const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:3030"
+    : "https://web-production-d139.up.railway.app";
 
 // 전체
 export async function getAllEvents() {}
@@ -8,7 +13,7 @@ export async function getAllEvents() {}
 export async function getRecentEvents(): Promise<EventThumbnail[] | undefined> {
   try {
     const recentEvents = fetch(
-      "https://web-production-d139.up.railway.app/v1/events?orderBy=latest&pageIndex=1&pageSize=10",
+      `${API_URL}/v1/events?orderBy=latest&pageIndex=1&pageSize=10`,
       { cache: "no-store" }
     )
       .then((rs) => rs.json())
@@ -21,7 +26,7 @@ export async function getRecentEvents(): Promise<EventThumbnail[] | undefined> {
 export async function getHotEvents(): Promise<EventThumbnail[] | undefined> {
   try {
     const hotEvents = fetch(
-      "https://web-production-d139.up.railway.app/v1/events?orderBy=views&pageIndex=1&pageSize=7",
+      `${API_URL}/v1/events?orderBy=views&pageIndex=1&pageSize=7`,
       { cache: "no-store" }
     )
       .then((rs) => rs.json())
@@ -41,9 +46,7 @@ export async function getHotEvents(): Promise<EventThumbnail[] | undefined> {
 // 상세정보
 export async function getEventDetail(id: number): Promise<Event | undefined> {
   try {
-    const detailEvent = fetch(
-      `https://web-production-d139.up.railway.app/v1/events/${id}`
-    )
+    const detailEvent = fetch(`${API_URL}/v1/events/${id}`)
       .then((rs) => rs.json())
       .then((data) => data.payload);
 
@@ -72,12 +75,12 @@ export async function getFilteredEvents(
   const pageSizeQuery = `pageSize=${pageSize}`;
 
   console.log(
-    `https://web-production-d139.up.railway.app/v1/events?${locationQuery}${categoryQuery}${costQuery}${startDateQuery}${endDateQuery}${orderByQuery}${pageIndexQuery}${pageSizeQuery}`
+    `${API_URL}/v1/events?${locationQuery}${categoryQuery}${costQuery}${startDateQuery}${endDateQuery}${orderByQuery}${pageIndexQuery}${pageSizeQuery}`
   );
 
   try {
     const filteredEvents = fetch(
-      `https://web-production-d139.up.railway.app/v1/events?${locationQuery}${categoryQuery}${costQuery}${startDateQuery}${endDateQuery}${orderByQuery}${pageIndexQuery}${pageSizeQuery}`,
+      `${API_URL}/v1/events?${locationQuery}${categoryQuery}${costQuery}${startDateQuery}${endDateQuery}${orderByQuery}${pageIndexQuery}${pageSizeQuery}`,
       { cache: "no-store" }
     )
       .then((rs) => rs.json())

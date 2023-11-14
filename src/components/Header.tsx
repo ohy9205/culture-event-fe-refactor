bad7303451098e50a95aea4d8295e0f5832d7087
yeon <ohy9205@naver.com>
2023-11-14 12:10:28 +0900

    fix: event메뉴 링크를 Link -> a 로 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,8 @@ const Header = () => {
     <header className="bg-slate-200 w-[1200px] h-[50px] flex justify-center items-center">
       <nav className="flex gap-5 text-xl">
         <Link href={"/"}>Home</Link>
-        <Link href={"/event"}>Event</Link>
-        {/* profile avatar 들어갈 자리 */}
-        <AuthLinks/>
+        <a href={"/event"}>Event</a>
+        <AuthLinks />
       </nav>
     </header>
   );

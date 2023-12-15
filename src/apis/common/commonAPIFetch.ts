fb260c14b416e4b0712e173aa32cd61b5e1db7fd
yeon <ohy9205@naver.com>
2023-12-15 13:28:30 +0900

    authorizedAPIFetch, APIFetch 함수 액션, 계산 분리

--- src/apis/common/commonAPIFetch.ts
+++ src/apis/common/commonAPIFetch.ts
@@ -2,38 +2,29 @@ import { APIResponse } from "@/src/types/APIResponse";
 import { getAccessToken, setAccessToken } from "@/src/utils/getAccessToken";
 
 type Method = "GET" | "POST" | "DELETE" | "PATCH";
+type FetchOption = {
+  method?: Method;
+  headers?: HeadersInit;
+  credentials?: RequestCredentials;
+  body?: Record<string, any> | null;
+  cache?: RequestCache;
+  next?: NextFetchRequestConfig;
+};
 
-// query는 url에 붙여서 같이 보내게 되고
-// body는 넣어줘야함
+// 액션
 export const authorizedAPIFetch = async (
   url: string,
   method: Method,
   body?: object
 ): Promise<APIResponse> => {
-  const accessToken = getAccessToken();
-  const rs = await fetch(url, {
-    method,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-    },
-    credentials: "include",
-    body: body ? JSON.stringify(body) : null,
-    next: {
-      revalidate: 36000,
-    },
-  });
+  const at = getAccessToken(); // 액션
+  const newData = await fetchData(
+    // 액션
+    url,
+    setAuthorizedFetchOption(method, at, body)
+  );
 
-  const data = await rs.json();
-
-  if (data.payload.at) {
-    setAccessToken(data.payload.at);
-  }
-
-  return {
-    status: rs.status,
-    ...data,
-  };
+  return newData;
 };
 
 export const APIFetch = async (
@@ -41,25 +32,82 @@ export const APIFetch = async (
   method: Method,
   body?: object
 ): Promise<APIResponse> => {
-  const rs = await fetch(url, {
+  const newData = await fetchData(
+    // 액션
+    url,
+    setUnauthorizedFetchOption(method, body)
+  );
+
+  return newData;
+};
+
+// 액션
+const fetchData = async (url: string, option: RequestInit) => {
+  const rs = await fetch(url, option); // 액션
+  const newData = await getResponseData(rs); // 계산
+  setToken(newData); // 액션
+
+  return newData;
+};
+
+// 계산
+const getResponseData = async (rs: Response): Promise<APIResponse> => {
+  const data = await rs.json();
+  return {
+    status: rs.status,
+    ...data,
+  };
+};
+
+// 계산
+const setAuthorizedFetchOption = (
+  method: Method,
+  token: string | null,
+  body?: Record<string, any> | null
+) => {
+  return makeFetchOption({
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    credentials: "include",
+    body,
+    next: {
+      revalidate: 3600,
+    },
+  });
+};
+
+// 계산
+const setUnauthorizedFetchOption = (
+  method: Method,
+  body?: Record<string, any> | null
+) => {
+  return makeFetchOption({
     method,
     headers: {
       "Content-Type": "application/json",
     },
     credentials: "include",
-    body: body ? JSON.stringify(body) : null,
+    body,
     next: {
-      revalidate: 36000,
+      revalidate: 3600,
     },
   });
-  const data = await rs.json();
+};
 
+// 계산
+const setToken = (data: APIResponse) => {
   if (data.payload.at) {
     setAccessToken(data.payload.at);
   }
+};
 
+// 계산
+const makeFetchOption = (options: FetchOption) => {
   return {
-    status: rs.status,
-    ...data,
+    ...options,
+    body: options.body ? JSON.stringify(options.body) : null,
   };
 };

d5be5b4fdebe15b2d30e94fd0f0b07424e100fee
yeon <ohy9205@naver.com>
2023-12-26 15:30:03 +0900

    signup폼 동작 캡슐화

--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -1,56 +1,38 @@
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { postSignup } from "../apis/auth/auth";
 import { responseHandler } from "../apis/common/commonAPIFetch";
-import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
-  const [email, setEmail] = useState("");
-  const [nick, setNick] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [form, setForm] = useState({
+    email: "",
+    nick: "",
+    password: "",
+    passwordConfirm: "",
+  });
   const [valid, setValid] = useState("");
-  const {
-    state: { isLoggedIn },
-  } = useAuthContext();
-
   const router = useRouter();
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      router.push("/");
-    }
-  }, [isLoggedIn]);
-
-  const signup = async () => {
-    const requestBody = {
-      email,
-      nick,
-      password,
-    };
-
-    const rs = await postSignup(requestBody);
-    if (rs) {
-      const handler = {
-        success: () => router.push("/signin"),
-        status403: () => setValid(rs.message),
-        status409: () => setValid(rs.message),
-      };
-      responseHandler(rs, handler);
-    }
-  };
+  const controller = {};
 
   return {
-    email,
-    setEmail,
-    password,
-    setPassword,
-    nick,
-    setNick,
-    passwordConfirm,
-    setPasswordConfirm,
-    signup,
-    valid,
+    get: () => ({ form, valid }),
+    change: (e: ChangeEvent<HTMLInputElement>) => {
+      setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    },
+    signup: async () => {
+      const { email, nick, password } = form;
+      const rs = await postSignup({ email, nick, password });
+
+      if (rs) {
+        const handler = {
+          success: () => router.push("/signin"),
+          status403: () => setValid(""),
+          status409: () => setValid(""),
+        };
+        responseHandler(rs, handler);
+      }
+    },
   };
 };
 

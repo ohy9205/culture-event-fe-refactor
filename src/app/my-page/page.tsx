864efbe3cf049334026934b5b56efd3154152ca7
yeon <ohy9205@naver.com>
2024-01-30 18:25:46 +0900

    ProtectRouter 사용중인 곳 제거

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,12 +1,7 @@
-import ProtectRouter from "@/src/components/UI/container/ProtectRouter";
 import MyInfo from "@/src/components/user/client/MyInfo";
 
 const MyPage = () => {
-  return (
-    <ProtectRouter>
-      <MyInfo />
-    </ProtectRouter>
-  );
+  return <MyInfo />;
 };
 
 export default MyPage;

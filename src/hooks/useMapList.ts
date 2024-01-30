ebcbe678127e7b66e494ae487daf6c3441ed15c7
yeon <ohy9205@naver.com>
2024-01-30 17:22:24 +0900

    필요없는 hooks 제거

--- src/hooks/useMapList.ts
+++ src/hooks/useMapList.ts
@@ -1,36 +1,13 @@
-import { useContext, useEffect, useState } from "react";
-import { responseHandler } from "../apis/common/responseHandler";
-import { getFilteredEvents } from "../apis/event/v2";
-import { FilterContext } from "../context/FilterContext";
-import { SimpleEvent } from "../types/events";
+"use client";
 
-const useMapList = () => {
-  const { filter } = useContext(FilterContext);
-  const [events, setEvents] = useState<SimpleEvent[]>([]);
-  const [curEvent, setCurEvent] = useState<SimpleEvent>();
+import { useState } from "react";
+import { Event, SimpleEvent } from "../types/events";
 
-  // 필터변경시 새로 데이터 패칭
-  useEffect(() => {
-    const fetchingData = async () => {
-      const rs = await getFilteredEvents(filter);
-
-      if (rs) {
-        const handler = {
-          success: () => {
-            setEvents(rs.payload.events);
-            setCurEvent(rs.payload.events[0]);
-          },
-        };
-        responseHandler(rs, handler);
-      }
-    };
-
-    fetchingData();
-  }, [filter]);
+const useMapList = (initCurEvent: Event) => {
+  const [curEvent, setCurEvent] = useState<SimpleEvent>(initCurEvent);
 
   return {
     data: {
-      events,
       curEvent,
     },
     changeCurrentEvent: (event: SimpleEvent) => setCurEvent(event),

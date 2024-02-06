db74f805c6044e422dab0d1f41fb919c16be3ca4
ohy9205 <ohy9205@naver.com>
2024-02-06 19:53:21 +0900

    ControlBox에서 filterContext를 쓰지 않고 바로 라우팅되게 처리

--- src/components/event/ControlBox.tsx
+++ src/components/event/ControlBox.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import useEventFilter from "@/src/hooks/useEventFilter";
 import { objectToQueryString } from "@/src/utils/common/objectController";
 import { evnetFilter } from "@/src/utils/data/eventFilter";
 import { useRouter } from "next/navigation";
-import { ChangeEvent, useEffect } from "react";
+import { ChangeEvent } from "react";
 
 const LOCATION = evnetFilter.location.options;
 const COST = evnetFilter.isFree.options;
@@ -13,21 +12,16 @@ const ORDER_BY = evnetFilter.orderBy.options;
 
 const SELECT_STYLE = `w-full h-full px-4 py-2 rounded-md bg-slate-100`;
 
-const ControlBox = () => {
-  const { filter, onFilterChange } = useEventFilter();
+const ControlBox = ({ query }: { query: Record<string, any> }) => {
   const router = useRouter();
 
   const changeFilter = (
     e: ChangeEvent<HTMLSelectElement | HTMLInputElement>
   ) => {
-    onFilterChange(e);
+    const { name, value } = e.target;
+    const clearQuery = cleanQuery({ ...query, [name]: value });
+    router.push(`/event?${objectToQueryString(clearQuery, "&")}`);
   };
-
-  useEffect(() => {
-    console.log("controlBox");
-    const query = objectToQueryString(filter, "&");
-    router.push(`/event?${query}`);
-  }, [filter]);
 
   return (
     <section className="w-full">
@@ -37,7 +31,9 @@ const ControlBox = () => {
             onChange={changeFilter}
             name="location"
             className={SELECT_STYLE}
-            value={filter.location || LOCATION[0].text}
+            // value={filter.location || locationOptions[0].text}
+            // value={filter.location || locationOptions[0].text}
+            value={query[locationName] || locationOptions[0].text}
           >
             {LOCATION.map((it) => (
               <option key={it.text} value={it.value}>
@@ -49,7 +45,8 @@ const ControlBox = () => {
             onChange={changeFilter}
             name="category"
             className={SELECT_STYLE}
-            value={filter.category || CATEGORY[0].text}
+            // value={filter.category || categoryOptions[0].text}
+            value={query[categoryName] || categoryOptions[0].text}
           >
             {CATEGORY.map((it) => (
               <option key={it.text} value={it.value}>
@@ -61,7 +58,8 @@ const ControlBox = () => {
             onChange={changeFilter}
             name="isFree"
             className={SELECT_STYLE}
-            value={filter.isFree || COST[0].text}
+            // value={filter.isFree || isFreeOptions[0].text}
+            value={query[isFreeName] || categoryOptions[0].text}
           >
             {COST.map((it) => (
               <option key={it.text} value={it.value}>
@@ -93,7 +91,8 @@ const ControlBox = () => {
             onChange={changeFilter}
             name="orderBy"
             className={SELECT_STYLE}
-            value={filter.orderBy || ORDER_BY[0].text}
+            // value={filter.orderBy || orderByOptions[0].text}
+            value={query[orderByName] || categoryOptions[0].text}
           >
             {ORDER_BY.map((it) => (
               <option key={it.text} value={it.value}>
@@ -113,5 +112,16 @@ const ControlBox = () => {
     </section>
   );
 };
+
+// 객체의 value가 "" | undefined | null 이면 제거하는 함수
+function cleanQuery(query: Record<string, any>): Record<string, any> {
+  const cleanedQuery: Record<string, any> = {};
+  Object.keys(query).forEach((key) => {
+    if (query[key] !== "") {
+      cleanedQuery[key] = query[key];
+    }
+  });
+  return cleanedQuery;
+}
 
 export default ControlBox;

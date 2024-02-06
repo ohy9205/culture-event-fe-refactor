2288bb3ee61d729fcbe4eeeaade6d5cd1b62d5d7
ohy9205 <ohy9205@naver.com>
2024-02-07 00:48:17 +0900

    검색어 필터에 디바운스 적용

--- src/hooks/useEventFilter.tsx
+++ src/hooks/useEventFilter.tsx
@@ -1,23 +1,41 @@
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { objectToQueryString } from "../utils/common/objectController";
+import useDebounce from "./useDebounce";
 
 const useEventFilter = (query: Record<string, any>) => {
   const router = useRouter();
+  const [filter, setFilter] = useState(query);
+  const [keyword, setKeyword] = useState(query.keyword || "");
+  const debouncedKeyword = useDebounce(keyword, 500);
 
   const onFilterChange = (
     e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>
   ) => {
     const { name, value } = e.target;
-    const clearQuery = cleanQuery({ ...query, pageIndex: 1, [name]: value });
-    router.push(`/event?${objectToQueryString(clearQuery, "&")}`);
+
+    if (name === "keyword") {
+      setKeyword(value);
+    } else {
+      setFilter({ ...filter, [name]: value });
+    }
   };
+
+  useEffect(() => {
+    const newQuery = {
+      ...filter,
+      keyword: debouncedKeyword,
+    };
+    const queryString = objectToQueryString(clearQuery(newQuery), "&");
+    router.push(`/event?${queryString}`);
+  }, [debouncedKeyword, router, keyword, filter]);
 
   return {
     onFilterChange,
   };
 };
 
-function cleanQuery(query: Record<string, any>): Record<string, any> {
+function clearQuery(query: Record<string, any>): Record<string, any> {
   const cleanedQuery: Record<string, any> = {};
   Object.keys(query).forEach((key) => {
     if (query[key] !== "") {

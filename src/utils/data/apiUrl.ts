c42bf6b9d17c11bfee16af7e55322f531d16501d
yeon <ohy9205@naver.com>
2024-02-10 01:35:31 +0900

    api base url https로 변경

--- src/utils/data/apiUrl.ts
+++ src/utils/data/apiUrl.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  "http://ec2-43-201-55-167.ap-northeast-2.compute.amazonaws.com:80";
+  "https://ec2-43-201-55-167.ap-northeast-2.compute.amazonaws.com:80";
 export const API_V1 = `${API_BASE_URL}/v1/events`;
 export const API_V2 = `${API_BASE_URL}/v2/events`;
 export const API_COMMENT = `${API_BASE_URL}/comment`;

96a56b559bbcc4f196c347211097852f4be4a7b0
ohy9205 <ohy9205@naver.com>
2024-02-13 21:50:24 +0900

    마이페이지 내 정보 영역 컴포넌트 분리

---
+++ src/components/user/MyInfo.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useAuth } from "@/src/hooks/useAuth";
+import React from "react";
+
+const MyInfo = () => {
+  const {
+    state: { user },
+  } = useAuth();
+
+  return (
+    <div className="w-full flex gap-4">
+      <div className="flex flex-col gap-4 justify-center">
+        <span className="flex justify-start">email: {user?.email}</span>
+        <span>nickname: {user?.nick}</span>
+      </div>
+    </div>
+  );
+};
+
+export default MyInfo;

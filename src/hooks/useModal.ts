84fb7c8708db8f7a097cebacc3e692f2c6dcb63f
yeon <ohy9205@naver.com>
2024-04-23 12:03:50 +0900

    ModalContext코드 리팩토링

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,6 +1,19 @@
-import { useContext } from "react";
-import { ModalContext } from "../context/ModalContext";
+import { ContextStore } from "../utils/globalStore/ContextSotre";
+
+type Context = {
+  isOpen: boolean;
+};
+
+const modalState = new ContextStore<Context>({
+  isOpen: false,
+});
 
 export const useModal = () => {
-  return useContext(ModalContext);
+  const [state, updateState] = modalState.useGlobalState(); // 선언할 때 할당한 context State가 내부에서 자동으로 인자로 들어감
+  return {
+    data: { isOpen: state.isOpen },
+    open: () => updateState({ isOpen: true }),
+    close: () => updateState({ isOpen: false }),
+    Provider: modalState.StoreProvider,
+  };
 };

8936f990ce3a71b1a7862ff451d395b723970218
ohy9205 <ohy9205@naver.com>
2024-04-24 22:34:20 +0900

    ZustandStore 수정

--- src/utils/globalStore/ZustandStore.tsx
+++ src/utils/globalStore/ZustandStore.tsx
@@ -1,17 +1,31 @@
+"use client";
+
+import React from "react";
 import { create } from "zustand";
 import { GlobalStoreAdapter } from "./Adapter";
 
+type State<T> = { state: T; updateState: (newState: Partial<T>) => void };
+
 export class ZustandStore<T> implements GlobalStoreAdapter<T> {
-  private useStore: any;
+  private useStore; // zustnad Create타입이 됨
 
   constructor(initialState: T) {
-    this.useStore = create((set: any) => ({
+    this.useStore = create<State<T>>((set: any) => ({
       state: initialState,
       updateState: (newState: Partial<T>) =>
-        set((prev: T) => ({ ...prev, ...newState })),
+        set((prevState: State<T>) => {
+          return { state: { ...prevState.state, ...newState } };
+        }),
     }));
   }
-  useGlobalState = () => this.useStore;
+
+  useGlobalState = (): [T, (newState: Partial<T>) => void] => {
+    const state = this.useStore((state: State<T>) => ({
+      state: state.state,
+      updateState: state.updateState,
+    }));
+    return [state.state, state.updateState];
+  };
 
   // zustand는 provider 없으므로 Fragment로감싸줌
   StoreProvider = ({ children }: { children: React.ReactNode }) => {

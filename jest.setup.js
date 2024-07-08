5dbe5cc36c86f9428870ae7d00cae8808fc79aa2
yeon <ohy9205@naver.com>
2024-07-09 02:32:50 +0900

    jest mock초기화 동작 정의

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,10 @@
+import { afterAll, afterEach, jest } from "@jest/globals";
 import "@testing-library/jest-dom";
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+afterAll(() => {
+  jest.resetAllMocks();
+});

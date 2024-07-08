f8ba2b945524803dafead724f96fd5fefe7fc1f3
yeon <ohy9205@naver.com>
2024-07-08 18:28:31 +0900

    objectController 테스트 코드 작성

---
+++ src/shared/lib/tests/objectController.test.ts
@@ -0,0 +1,35 @@
+import { expect, it } from "@jest/globals";
+import { deleteObjectKeys, objectToQueryString, replaceObjectKey } from "..";
+
+it("인자로 받은 key값에 해당하는 데이터를 변경한다", () => {
+  const obj = {
+    a: "1",
+    b: "2",
+    c: "3",
+  };
+  const resultObj = replaceObjectKey(obj, "b", "test");
+
+  expect(resultObj).toEqual({ a: "1", b: "test", c: "3" });
+});
+
+it("인자로 받은 key배열에 해당하는 데이터들을 삭제한다", () => {
+  const obj = {
+    a: "1",
+    b: "2",
+    c: "3",
+  };
+  expect(deleteObjectKeys(obj, ["a"])).toEqual({ b: "2", c: "3" });
+  expect(deleteObjectKeys(obj, ["b", "c"])).toEqual({ a: "1" });
+});
+
+it("인자로 받은 구분자를 이용해 객체를 쿼리 스트링으로 변경한다.", () => {
+  const obj = {
+    name: "yeon",
+    age: "29",
+    gender: "female",
+  };
+
+  expect(objectToQueryString(obj, "&")).toEqual(
+    "name=yeon&age=29&gender=female"
+  );
+});

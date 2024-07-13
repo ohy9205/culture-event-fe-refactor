f4ca469bef03d46941c76fe909b67e06a16f7946
yeon <ohy9205@naver.com>
2024-07-14 00:27:09 +0900

    MSW 셋업

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,18 @@
-import { afterAll, afterEach, jest } from "@jest/globals";
+import { afterAll, afterEach, beforeAll, jest } from "@jest/globals";
 import "@testing-library/jest-dom";
+import { server } from "./__mocks__/server";
+
+beforeAll(() => {
+  server.listen();
+});
 
 afterEach(() => {
+  server.resetHandlers();
   jest.clearAllMocks();
 });
 
 afterAll(() => {
+  server.close();
   jest.resetAllMocks();
+  jest.useRealTimers();
 });

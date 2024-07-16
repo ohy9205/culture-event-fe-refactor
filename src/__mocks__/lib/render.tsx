3908c1f40d6d6a688eda749d1f22016335ce3451
yeon <ohy9205@naver.com>
2024-07-17 01:24:25 +0900

    테스트용 Provider를 감싼 render함수 생성

---
+++ src/__mocks__/lib/render.tsx
@@ -0,0 +1,30 @@
+import { jest } from "@jest/globals";
+import { RenderOptions, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { AppRouterContextProviderMock } from "./app-router-context-provider-mock";
+
+export default async (
+  ui: React.ReactNode,
+  options?: Omit<RenderOptions, "queries">
+) => {
+  const router = {
+    back: jest.fn(),
+    forward: jest.fn(),
+    push: jest.fn(),
+    replace: jest.fn(),
+    refresh: jest.fn(),
+    prefetch: jest.fn(),
+  };
+
+  const user = userEvent.setup();
+
+  return {
+    user,
+    ...render(
+      <AppRouterContextProviderMock router={router}>
+        {ui}
+      </AppRouterContextProviderMock>,
+      options
+    ),
+  };
+};

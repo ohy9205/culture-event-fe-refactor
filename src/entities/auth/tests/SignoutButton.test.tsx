3d2c4f804b4ef9a5d453513aee18f2a1fac632e9
yeon <ohy9205@naver.com>
2024-07-17 02:39:36 +0900

    SignoutButton 테스트 코드 작성

---
+++ src/entities/auth/tests/SignoutButton.test.tsx
@@ -0,0 +1,12 @@
+import { render } from "@/src/__mocks__/lib";
+import { expect, it } from "@jest/globals";
+import { screen, waitFor } from "@testing-library/dom";
+import { SignoutButton } from "..";
+
+it("로그아웃에 성공하면 '/'페이지로 이동한다", async () => {
+  const { user } = await render(<SignoutButton />);
+
+  await user.click(screen.getByText("로그아웃"));
+
+  await waitFor(() => expect(window.location.href).toBe("/"));
+});
